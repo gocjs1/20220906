81e4bd38394cfab0cc00be4615a6307888fb7757
gocjs1@stu.kmu.ac.kr <gocjs1@stu.kmu.ac.kr>
2022-09-06 10:40:11 +0900

    과제2 투명한 다이아 모양 만들기

--- 20220906/star.c
+++ 20220906/star.c
@@ -7,7 +7,32 @@ int main() {
 	scanf_s("%d", &input);
 
 	for (int i = 0; i < input; i++)
-		printf("**");
+	{
+		for(int j = input - i; j > 0; j--)
+			printf("*");
+		for (int k = i; k > 0; k--)
+			printf(" ");
+		for (int k = i; k > 0; k--)
+			printf(" ");
+		for (int j = input - i; j > 0; j--)
+			printf("*");		
 
+		printf("\n");
+	}
+
+	for (int i = 0; i < input; i++)
+	{
+		for (int j = i + 1; j > 0; j--)
+			printf("*");
+		for (int k = input - (i+1) ; k > 0; k--)
+			printf(" ");
+		for (int k = input - (i + 1); k > 0; k--)
+			printf(" ");
+		for (int j = i + 1; j > 0; j--)
+			printf("*");
+
+		printf("\n");
+	}
+	    
 	return 0;
 }
